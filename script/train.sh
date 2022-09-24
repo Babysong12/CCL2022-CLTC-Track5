e58f170f0940bfecbb7f3e4d9d588c08ed8937b0
Babysong12 <114163497+Babysong12@users.noreply.github.com>
2022-09-24 19:35:39 +0800

    Add files via upload

---
+++ script/train.sh
@@ -0,0 +1,4 @@
+python ../train.py --outdir ../save_model_fluency_electra_23 \
+--train_path ../data_qe/train.json \
+--valid_path ../data_qe/valid.fluency.json \
+--model_name_or_path hfl/chinese-electra-180g-base-discriminator
